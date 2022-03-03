30aa870e976e5af41034690307bcf2702a6182e3
wolfiediscord <wolfiediscord@gmail.com>
2022-03-02 23:06:32 -0600

    Add invite command

---
+++ commands/invite.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ApplicationCommandRegistry, Command } = require('@sapphire/framework');
+
+module.exports = class InviteCommand extends Command {
+	registerApplicationCommands(registry) {
+		registry.registerChatInputCommand(
+		{
+			name: 'invite',
+			description: 'Invite the bot to your server!'
+		}, {
+			guildIds: []
+		}
+		);
+	};
+
+	async chatInputRun(interaction) {
+		let inviteEmbed = {
+			title: "Invite Wolf Tags",
+			description: "[Invite Wolf Tags to your server!](https://discord.com/oauth2/authorize?client_id=888666209787199509&scope=bot%20applications.commands&permissions=0)"
+		};
+		return interaction.reply({embeds: [inviteEmbed], ephemeral: true});
+	};
+};
